86fdda67e2b12d981fcc4853c2b58d434dadee6a
jojoluzifer22 <jojoluzifer22@gmail.com>
2025-08-18 12:52:59 +0700

    chore: add api / for test Railway

--- index.js
+++ index.js
@@ -21,93 +21,14 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 
-
-// create JWT
-// const generateToken = (user) => {
-//   return jwt.sign({ id: user.id, name: user.name, email: user.email, role: user.role, dob:user.dob }, process.env.JWT_SECRET, {
-//     expiresIn: '3h',
-//   });
-// };
-
-// const authMiddleware = (req, res, next) => {
-//   const token = req.cookies.token;
-//   if (!token) return res.status(401).json({ message: 'Unautherized' });
-//   try {
-//     const user = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = user;
-//     next();
-//   } catch (err) {
-//     res.status(401).json({ message: 'Invalid Token' });
-//   }
-// };
-
-// app.post('/login', (req, res) => {
-//   const { username, password } = req.body;
-//   const user = users.find(
-//     (u) => u.username === username && u.password === password
-//   );
-
-//   if (!user) return res.status(401).json({ message: 'Invalid credentials' });
-
-//   const token = generateToken(user);
-//   res.cookie('token', token, {
-//     httpOnly: true,
-//     secure: false,
-//     sameSite: 'lax',
-//   });
-
-//   res.json({ user: { id: user.id, name: user.name  , email: user.email, dob: user.dob, role: user.role} });
-// });
-// app.get('/test', (req, res) => {
-//   res.json({ message: 'Test Backend' });
-// });
-
-// app.get('/me', authMiddleware, (req, res) => {
-//   res.json({ user: req.user });
-// });
-
-// app.post('/logout', (req, res) => {
-//   res.clearCookie('token');
-//   res.json({ message: 'Loggedd' });
-// });
-
-// app.get("/users" , (req,res)=>{
-
-//     let {page,limit,search, role } = req.query;
-
-//     page = parseInt(page);
-//     limit = parseInt(limit);
-
-//     let filtered = users.filter(user => {
-//         const matchSearch = search 
-//         ? user.name.toLowerCase().includes(search.toLowerCase())
-//         : true;
-//         const matchRole = role 
-//         ? user.role === role 
-//         : true;
-
-//         return matchRole && matchSearch;
-//     });
-
-//     const total = filtered.length;
-
-//     const startIndex = (page - 1) * limit;
-//     const paginated = filtered.slice(startIndex, startIndex+ limit);
-
-//     res.json({
-//         data: paginated,
-//         total
-//     });
-
-
-// });
-
-// app.use(profileRoutes);
-
 // Routes
 app.use('/auth' , authRoutes);
 app.use('/profile' , profileRoutes);
 app.use('/users' , userRoutes);
+
+app.get('/' , (req,res)=>{
+  res.json({message: "Hello from express on Railway"});
+});
 
 
 app.listen(3000, () => {
