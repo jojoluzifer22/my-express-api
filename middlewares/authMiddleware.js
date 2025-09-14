5023959e84af316c1dd4a7c66d687b339e677992
jojoluzifer22 <jojoluzifer22@gmail.com>
2025-09-14 11:29:37 +0700

    fix(auth): correct JWT secret import in Express middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
-const jwtSecret = require('../config/env');
+const {jwtSecret} = require('../config/env');
 
 
 const authMiddleware = (req, res, next)=>{
-
+    console.log('jwtSecret type:', typeof jwtSecret, 'value:', jwtSecret?.toString?.().slice?.(0,20));
     const token = req.cookies.token;
     if(!token) return res.status(401).json({message: "No token provided"});
 
--- utils/token.js
+++ utils/token.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const { jwtSecret} = require("../config/env");
 
 const generateToken = (user)=>{
+    console.log('jwtSecret type:', typeof jwtSecret, 'value:', jwtSecret?.toString?.().slice?.(0,20));
     return jwt.sign(
         { id: user.id, name: user.name, email: user.email, role: user.role, dob: user.dob} ,
         jwtSecret,
